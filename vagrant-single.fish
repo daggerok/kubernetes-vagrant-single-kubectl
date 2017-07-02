d89a916136a12a3d56ccf1a2a21b9765c68b4fcf
Maksim Kostromin <daggerok@gmail.com>
2017-07-03 01:30:04 +0300

    Update vagrant-single.fish

--- vagrant-single.fish
+++ vagrant-single.fish
@@ -11,7 +11,19 @@ kubectl config set-context vagrant-single \
 
 kubectl config use-context vagrant-single
 
-# kubectl get nodes
 # kubectl run webapp --image=nginx --port=80
 # kubectl get deployments
-# kubectl get pods
+
+# kubectl get nodes
+# NAME          STATUS    AGE       VERSION
+# 172.17.4.99   Ready     55m       v1.5.4+coreos.0
+# set -l host 172.17.4.99 
+
+# kubectl expose deployment webapp --target-port=80 --type=NodePort
+# kubectl get service webapp                                                                                                                                               01:24:03
+# NAME      CLUSTER-IP   EXTERNAL-IP   PORT(S)        AGE
+# webapp    10.3.0.231   <nodes>       80:32175/TCP   6m
+# set -l port 32175
+
+# http "$host:$port"
+# nginx respose...
